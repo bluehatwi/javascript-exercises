669c68eb210427e84a900ec4b33737e9b8978c2a
hari <197885045+bluehatwi@users.noreply.github.com>
2025-12-23 18:42:18 +1030

    doneee

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    result = "";
+    if (num < 0){
+        return 'ERROR';
+    }
+    for (let i = 0; i < num; i++){
+        result = result.concat(string);
+    }
+    return result;
 };
-
+let str = repeatString('hey', 3);
+console.log(str);
 // Do not edit below this line
 module.exports = repeatString;
