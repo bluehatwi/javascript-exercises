650ddb5795e4b97678a5da6c7e5d79f9f6635f9f
hari <197885045+bluehatwi@users.noreply.github.com>
2025-12-23 18:52:56 +1030

    doneeee

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const length = string.length;
+    let reversed = ""
+    for (let i = length-1; i > -1; i--){
+        reversed = reversed + string[i];
+    }
+    return reversed;
 };
 
+let ayo = reverseString('hello there');
+console.log(ayo);
 // Do not edit below this line
 module.exports = reverseString;
