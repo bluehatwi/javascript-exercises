870e5d3ac700a852de227b069b0a1b0e1d868b12
hari <197885045+bluehatwi@users.noreply.github.com>
2025-12-24 20:57:43 +1030

    doneeeee

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let num = ((temp-32)*(5/9));
+  return +num.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let num = ((temp*(9/5))+32);
+  return +num.toFixed(1);
 };
+
+console.log(convertToCelsius(100));
 
 // Do not edit below this line
 module.exports = {
