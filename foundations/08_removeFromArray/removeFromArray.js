20e60ce6bc97a14eceeb8a5d11696856a2d42820
hari <197885045+bluehatwi@users.noreply.github.com>
2025-12-24 18:55:39 +1030

    doneeeee

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let newArray = [];
+    const length = array.length;
+    for (let i = 0; i < length; i++){
+        if (!args.includes(array[i])){
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
 };
-
+console.log(removeFromArray([1, 2, 3, 4], 3));
 // Do not edit below this line
 module.exports = removeFromArray;
