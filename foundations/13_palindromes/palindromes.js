77e49e6b5de8dac00ddece3de53f04c70305f0b6
hari <197885045+bluehatwi@users.noreply.github.com>
2026-01-06 19:20:57 +1030

    doneeee

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (stringgg) {
+    // - Use regex to remove anything that is NOT a letter (a-z) or number (0-9)
+    const processedString = stringgg
+                            .toLowerCase()
+                            .replace(/[^a-z0-9]/g, "");
+                            
+    const lengthh = processedString.length;
+    for (let i = 0; i < lengthh / 2; i++) {
+        if (processedString[i] !== processedString[lengthh - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
