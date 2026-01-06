642b1a0770e3543f58137c25f8accf6a6afddca6
hari <197885045+bluehatwi@users.noreply.github.com>
2026-01-06 18:47:22 +1030

    doneeeee

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(Numbers) {
+  return Numbers.reduce(function(totalll, currentNum){
+    return totalll + currentNum;
+  }, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(Numbers) {
+  return Numbers.reduce(function(totalll, currentNum){
+    return totalll*currentNum;
+  }, 1)
 };
 
-const power = function() {
-	
+const power = function(base,exponent) {
+  return Math.pow(base, exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n === 0 || n === 1){
+    return 1;
+  }
+  let result = 1;
+  for (let i = 2; i <=n; i++){
+    result = result * i;
+  }
+  return result;
 };
 
 // Do not edit below this line
