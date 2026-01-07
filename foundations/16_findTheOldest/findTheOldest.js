f8cfd021a7c7ca3e599c094c0dd57d4aecb60aa5
hari <197885045+bluehatwi@users.noreply.github.com>
2026-01-07 13:05:26 +1030

    doneeeeee

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(peeps) {
+    const sorted = peeps.sort(function(a,b){
 
+        const currentYear = new Date().getFullYear();
+        let aDeath;
+        let bDeath;
+        if (!a.yearOfDeath){
+            aDeath =  currentYear;
+        } else {
+            aDeath = a.yearOfDeath;
+        }
+        if (!b.yearOfDeath){
+            bDeath =  currentYear;
+        } else {
+            bDeath = b.yearOfDeath;
+        }
+        const aAge = aDeath - a.yearOfBirth;
+        const bAge = bDeath - b.yearOfBirth;
+        return aAge - bAge;
+    });
+   return sorted[sorted.length - 1];
 };
 
 // Do not edit below this line
